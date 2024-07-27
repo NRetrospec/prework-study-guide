38ecb117bb60ee33b0736cd1246d9559e7fff483
NRetrospec <marquisej.galo@gmail.com>
2024-07-26 22:51:38 -0400

    Update main.js

--- prework-study-guide/scripts/main.js
+++ prework-study-guide/scripts/main.js
@@ -1,11 +1,22 @@
-const myImage = document.querySelector("img");
-
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
-
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];;
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+for(let x = 0; x < topics.length; x++)
+    console.log(topics[x]); }
+function selectTopics() {
+if (randomTopic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (randomTopic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (randomTopic === 'Git') {
+  console.log("Let's study Git!");
+} else if (randomTopic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+} 
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+console.log('Which topic should we study first?');
+selectTopics()
